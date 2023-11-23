4aadc2adadcbc029dec6985d5a2532fe1c2bdbbf
manuelseisl <manuel.seisl@micado.cc>
2023-11-23 18:14:35 +0100

    added pages

--- src/app/de/[slug]/page.js
+++ src/app/de/[slug]/page.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Page({ params }) {
 	return (
 		<>
 			<Link href="/de/test">Test</Link>
@@ -9,6 +9,7 @@ export default function Home() {
 			<Link href="/de/test3">Test3</Link>
 			<Link href="/de/test4">Test4</Link>
 			<Link href="/de/test5">Test5</Link>
+			<p>{params.slug}</p>
 		</>
 	);
 }
