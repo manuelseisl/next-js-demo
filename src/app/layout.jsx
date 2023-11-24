23f1583648020eb432783578c387e588986eaece
Manuel Seisl <manuel.seisl@micado.cc>
2023-11-24 13:20:11 +0100

    added gtm from next

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,29 +1,10 @@
-import GTMProvider from "@/components/gtmprovider";
+import { GoogleTagManager } from "@next/third-parties/google";
 
-/* eslint-disable @next/next/next-script-for-ga */
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<head>
-				<script
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-							new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-							j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-							'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-							})(window,document,'script','dataLayer','GTM-WDH54DJD');`
-					}}
-				></script>
-			</head>
-			<body>
-				{children}
-				<GTMProvider />
-				<noscript
-					dangerouslySetInnerHTML={{
-						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WDH54DJD" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-					}}
-				></noscript>
-			</body>
+			<body>{children}</body>
+			<GoogleTagManager gtmId="GTM-WDH54DJD" />
 		</html>
 	);
 }
