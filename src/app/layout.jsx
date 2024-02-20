2e9e6dad7a93ff795cd8069af7773d5b839ffd3f
manuelseisl <manuel.seisl@micado.cc>
2024-02-20 20:42:20 +0100

    updated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,6 +24,13 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WDH54DJD');
+
+gtag('consent', 'update', {
+    'ad_storage': 'granted',
+    'ad_user_data': 'granted',
+    'ad_personalization': 'granted',
+    'analytics_storage': 'granted'
+  });
   `
 					}}
 				/>
