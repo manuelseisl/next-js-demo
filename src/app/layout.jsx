a40341fd0acec522d30ec488f72b15d24723e3b4
manuelseisl <manuel.seisl@micado.cc>
2024-02-20 20:37:32 +0100

    fixed style prop

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -35,7 +35,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 						src="https://www.googletagmanager.com/ns.html?id=GTM-WDH54DJD"
 						height="0"
 						width="0"
-						style="display:none;visibility:hidden"
+						style={{ display: "none", visibility: "hidden" }}
 					></iframe>
 				</noscript>
 			</body>
